85cd6d9b4f6f0e4fec74d8f6e1922b589d8eead4
lookaffe <lookaffe@gmail.com>
2018-10-15 16:34:16 +0200

    Update Software/Room1/G11_Xilofono/G11_Xilofono_Mudbus_v2/G11_Xilofono_Mudbus_v2.ino

--- Software/Room1/G11_Xilofono/G11_Xilofono_Mudbus_v2/G11_Xilofono_Mudbus_v2.ino
+++ Software/Room1/G11_Xilofono/G11_Xilofono_Mudbus_v2/G11_Xilofono_Mudbus_v2.ino
@@ -1,15 +1,15 @@
 
 // TEENSY 3.5
 /*
- * Pin Tasto PLAY 19
- * Pin Tasto RESET 35
- * Pin Luce PLAY 21
- * Pin Luce RESET 33
- * Pin Pulsanti Elettrovalvole 17, 15, 22, 20, 18, 16
- * Pin Pulsanti Martelletti 25, 24, 26, 28, 30, 32
- * Pin Relay Elettrovalvole 0, 2, 4, 6, 8, 7
- * Pin Servo 23
- */
+   Pin Tasto PLAY 19
+   Pin Tasto RESET 35
+   Pin Luce PLAY 21
+   Pin Luce RESET 33
+   Pin Pulsanti Elettrovalvole 17, 15, 22, 20, 18, 16
+   Pin Pulsanti Martelletti 25, 24, 26, 28, 30, 32
+   Pin Relay Elettrovalvole 0, 2, 4, 6, 8, 7
+   Pin Servo 23
+*/
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -18,18 +18,23 @@
 
 #define SENNUM  14 //total amount of sensors
 #define ACTNUM  0 //total amount of actuators
-#define DEVNUM  10 //total amount of internal devices
+#define DEVNUM 9 //total amount of internal devices
 
 #define ALWAYSACTIVE 1 //1 if the game is always active
 
-uint8_t mac[] = {0x04, 0xE9, 0xE5, 0x04, 0xE9, 0xE5}; //Dipende da ogni dispositivo, da trovare con T3_readmac.ino (Teensy) o generare (Arduino)
-uint8_t ip[] = {10, 0, 0, 101};                           //This needs to be unique in your network - only one puzzle can have this IP
+// per questo gioco------
+#define VALNUM 6
+#define HAMNUM 6
+// ----------------------
+
+uint8_t mac[] = {0x04, 0xE9, 0xE5, 0x04, 0xE9, 0xE4}; //Dipende da ogni dispositivo, da trovare con T3_readmac.ino (Teensy) o generare (Arduino)
+uint8_t ip[] = {10, 0, 0, 111};                           //This needs to be unique in your network - only one puzzle can have this IP
 
 //Modbus Registers Offsets (0-9999)
 const int STATE = 0;
 const int SENSORS[SENNUM] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}; // 1 playButton, 2:7 valvButton, 8 resetButton, 9:14 hamButton
 const int ACTUATORS[ACTNUM] = {};
-const int DEVICES[DEVNUM] = {51, 52, 53, 54, 55, 56, 57, 58, 59, 60}; // 51 servo, 52 playLight, 53 resetLight, 54:60 valvRelay
+const int DEVICES[DEVNUM] = {51, 52, 53, 54, 55, 56, 57, 58, 59}; // 52 playLight, 54:60 valvRelay 53 resetLight, 51 servo
 const int RESET = 100;
 const int ACTIVE = 124;
 
@@ -39,211 +44,195 @@ bool triggered = false; // has the control room triggered some actuator?
 bool gameActivated = ALWAYSACTIVE; // is the game active?
 
 //Used Pins
-const int sensPins[SENNUM] = {19, 17, 15, 22, 20, 18, 16, 35, 25, 24, 26, 28, 30, 32}; // Capacitor
+const int sensPins[SENNUM] = {19, 17, 15, 22, 20, 18, 16, 35, 25, 24, 26, 28, 30, 32}; // // 19 playButton, 17:16 valvButton, 35 resetButton, 25:32 hammButton
 const int actPins[ACTNUM] = {}; // relay
-const int devPins[DEVNUM] = {23, 21, 33, 0, 2, 4, 6, 8, 7} ;
+const int devPins[DEVNUM] = {21, 0, 2, 4, 6, 8, 7, 33, 23} ; // , 21 playLight, 0:7 valvRelay, 33 resetLight, 23 servo
 
-int sensStatus[SENNUM] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+int sensStatus[SENNUM] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
 int lightPin[SENNUM] = {14, 15, 16, 17, 18, 19, 20, 21, 22, 23} ;
 int sequence[SENNUM] = {0, 1, 1, 0, 0, 0 , 1, 1, 1, 0};      //the right sequence
 int yourSequence[SENNUM] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};   //user sequence
 
+Bounce button0 = Bounce(sensPins[0], 100); //Pin button
 
- 
-const int MAX_LEVEL = 100;
-int sequence[MAX_LEVEL];
-int your_sequence[MAX_LEVEL];
-int level = 4;
+// per questo gioco------
+const int playLightPin = 21;
+const int playButtonPins = 19;
+int playState = 0;
 
-int velocity = 500;
+const int resetLightPin = 33;
+const int resetvalvButtonPins = 35;
+int resetState = 0;
+
+const int valvPins[VALNUM] = {0, 2, 4, 6, 8, 7};
+const int valvButtonPins[VALNUM] = {17, 15, 22, 20, 18, 16};
+int valvButtonState[VALNUM] = {0, 0, 0, 0, 0, 0};
+
+const int hammButtonPins[HAMNUM] = {25, 24, 26, 28, 30, 32};
+int hammButtonState [HAMNUM] = {0, 0, 0, 0, 0, 0};
+
+const long fluxTime = 2000; // 2900 per 0,5 cl
+
+int waterSteps[VALNUM] = {1, 2, 0, 3, 2, 1};  //quantità d'acqua per le note giuste
+int yourWaterSteps[VALNUM] {0, 0, 0, 0, 0, 0};
+//-----------------------
+
+//ModbusIP object
+Mudbus Mb;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A4, INPUT);
+  // reset for Ethernet Shield
+  pinMode(9, OUTPUT);
+  digitalWrite(9, LOW); // reset the WIZ820io
+  delay(1000);
+  digitalWrite(9, HIGH); // release the WIZ820io
 
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(6, OUTPUT);
+  Ethernet.begin(mac, ip);
+  delay(5000);
 
-  pinMode(5, OUTPUT);
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
 
-  digitalWrite(5, LOW);
+  // Initial game state
+  puzzleSolved = false;
+  Mb.R[ACTIVE] = gameActivated;
 
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(6, LOW);
+  //Set Pin mode
+  for (int i = 0; i < VALNUM; i++) {
+    pinMode(valvPins[i], OUTPUT);
+    pinMode(valvButtonPins[i], INPUT);
+    digitalWrite(valvPins[i], LOW);  // LOW se uso SSRelay
+  }
+  for (int i = 0; i < HAMNUM; i++) {
+    pinMode(hammButtonPins[i], INPUT);
+  }
+  pinMode(playLightPin, OUTPUT);
+  digitalWrite(playLightPin, LOW);
+  pinMode(resetLightPin, OUTPUT);
+  digitalWrite(resetLightPin, LOW);
 }
 
 void loop()
 {
-//  Serial.print("A0: "); Serial.println(digitalRead(A0));
-//  Serial.print("A1: "); Serial.println(digitalRead(A1));
-//  Serial.print("A2: "); Serial.println(digitalRead(A2));
-//  Serial.print("A3: "); Serial.println(digitalRead(A3));
-//  delay(velocity);
-  if (level == 4)
-    generate_sequence();//generate a sequence;
+  Mb.Run();
+  listenFromEth();
+  if (!triggered) {
+    gameUpdate();
+    isPuzzleSolved();
+  }
+  printSteps();
+  //printRegister();
+}
 
-  if (deButton(A4, LOW)) //If start button is pressed
-  {
-    show_sequence();    //show the sequence
-    get_sequence();     //wait for your sequence
+void gameUpdate() {
+  fillTheGlasses();
+}
+
+void fillTheGlasses() {
+  for (int i = 0; i < VALNUM; i++) {
+    valvButtonState[i] = digitalRead(valvButtonPins[i]);
+    Mb.R[SENSORS[i + 1]] = !valvButtonState[i];
+        if (!valvButtonState[i]) {
+      openValv(i);
+      yourWaterSteps[i] = yourWaterSteps[i] +1;
+    }
   }
 }
 
-void show_sequence()
-{
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(6, LOW);
+void openValv(int v) {
+  digitalWrite(valvPins[v], HIGH); // HIGH se uso SSRelay
+  delay(fluxTime);
+  digitalWrite(valvPins[v], LOW);
+}
 
-  for (int i = 0; i < level; i++)
-  {
-    digitalWrite(sequence[i], HIGH);
-    delay(velocity);
-    digitalWrite(sequence[i], LOW);
-    delay(200);
+void isPuzzleSolved() {
+  Mb.R[STATE] = puzzleSolved;
+}
+
+// Azione su ricezione comando "trigger"
+void trigger(int s, boolean trig) {
+  Mb.R[ACTUATORS[s]] = trig;
+  digitalWrite(s, trig);
+  delay(10);
+}
+
+// Resetta il gioco
+void reset() {
+  for (int i = 0; i < ACTNUM ; i++) {
+    trigger(actPins[i], LOW);
+  }
+  triggered = false;
+  for (int i = 0; i < SENNUM ; i++) {
+    sensStatus[i] = LOW;
+    Mb.R[SENSORS[i]] = sensStatus[i];
+  }
+  for (int i = 0; i < DEVNUM ; i++) {
+    digitalWrite(devPins[i], LOW);
+    Mb.R[DEVICES[i]] = LOW;
+  }
+  puzzleSolved = false;
+  Mb.R[STATE] = puzzleSolved;
+  Mb.R[RESET] = LOW;
+  if (!ALWAYSACTIVE) {
+    gameActivated = false;
+    Mb.R[ACTIVE] = gameActivated;
   }
 }
 
-void get_sequence()
-{
-  int flag = 0; //this flag indicates if the sequence is correct
-
-  for (int i = 0; i < level; i++)
-  {
-    flag = 0;
-    while (flag == 0)
-    {
-      if (deButton(A3, HIGH))
-      {
-        digitalWrite(6, HIGH);
-        your_sequence[i] = 5;
-        flag = 1;
-        delay(velocity);
-        if (your_sequence[i] != sequence[i])
-        {
-          wrong_sequence();
-          return;
-        }
-        digitalWrite(6, LOW);
-      }
-
-      if (deButton(A2, HIGH))
-      {
-        digitalWrite(4, HIGH);
-        your_sequence[i] = 4;
-        flag = 1;
-        delay(velocity);
-        if (your_sequence[i] != sequence[i])
-        {
-          wrong_sequence();
-          return;
-        }
-        digitalWrite(4, LOW);
-      }
-
-      if (deButton(A1, HIGH))
-      {
-        digitalWrite(3, HIGH);
-        your_sequence[i] = 3;
-        flag = 1;
-        delay(velocity);
-        if (your_sequence[i] != sequence[i])
-        {
-          wrong_sequence();
-          return;
-        }
-        digitalWrite(3, LOW);
-      }
-
-      if (deButton(A0, HIGH))
-      {
-        digitalWrite(2, HIGH);
-        your_sequence[i] = 2;
-        flag = 1;
-        delay(velocity);
-        if (your_sequence[i] != sequence[i])
-        {
-          wrong_sequence();
-          return;
-        }
-        digitalWrite(2, LOW);
+void listenFromEth() {
+  if (Mb.R[RESET]) reset();
+  else {
+    for (int i = 0; i < SENNUM ; i++) {
+      sensStatus[i] = Mb.R[SENSORS[i]];
+    }
+    triggered = 0;
+    for (int i = 0; i < ACTNUM ; i++) {
+      trigger(actPins[i], Mb.R[ACTUATORS[i]]);
+      triggered = triggered || Mb.R[ACTUATORS[i]];
+    }
+    for (int i = 0; i < DEVNUM ; i++) {
+      switch (i) {
+        case 0: // play light
+          digitalWrite(playLightPin, Mb.R[DEVICES[i]]);
+          Serial.println ("accade");
+          break;
+        case 7: //reset light
+          digitalWrite(resetLightPin, Mb.R[DEVICES[i]]);
+          break;
+        case 8: // servo
+          digitalWrite(devPins[i], Mb.R[DEVICES[i]]);
+          break;
+        default: // valv c
+          if (Mb.R[DEVICES[i]]) openValv(i - 1);
+          break;
       }
 
     }
-  }
-  right_sequence();
-}
-
-void generate_sequence()
-{
-  randomSeed(millis()); //in this way is really random!!!
-
-  for (int i = 0; i < MAX_LEVEL; i++)
-  {
-    sequence[i] = random(2, 5);
+    puzzleSolved = Mb.R[STATE];
+    if (Mb.R[STATE]) triggered = Mb.R[STATE];
+    gameActivated = Mb.R[ACTIVE];
   }
 }
-void wrong_sequence()
-{
-  for (int i = 0; i < 3; i++)
-  {
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(6, HIGH);
-    delay(250);
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(6, LOW);
-    delay(250);
+
+void printSteps() {
+for (int i = 0; i < VALNUM; i++) {
+    Serial.print("Glass "); Serial.print(i); Serial.print(": "); Serial.print(yourWaterSteps[i]); Serial.print(" of  "); Serial.println(waterSteps[i]);
   }
-  level = 4;
 }
 
-void right_sequence()
-{
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(6, LOW);
-  delay(250);
-
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(6, HIGH);
-  delay(500);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(6, LOW);
-  delay(500);
-
-//  if (level < MAX_LEVEL);
-//  level++;
- digitalWrite(5, HIGH);
-
+void printRegister() {
+  Serial.print("STATE (reg "); Serial.print(STATE); Serial.print(") - val:  "); Serial.println(Mb.R[STATE]);
+  for (int i = 0; i < SENNUM; i++) {
+    Serial.print("SENSORS "); Serial.print(i); Serial.print(" (reg "); Serial.print(SENSORS[i]); Serial.print(") - val:  "); Serial.println(Mb.R[SENSORS[i]]);
+  }
+  for (int i = 0; i < ACTNUM; i++) {
+    Serial.print("ACTUATORS "); Serial.print(i); Serial.print(" (reg "); Serial.print(ACTUATORS[i]); Serial.print(") - val:  "); Serial.println(Mb.R[ACTUATORS[i]]);
+  }
+  for (int i = 0; i < DEVNUM; i++) {
+    Serial.print("DEVICES "); Serial.print(i); Serial.print(" (reg "); Serial.print(DEVICES[i]); Serial.print(") - val:  "); Serial.println(Mb.R[DEVICES[i]]);
+  }
+  Serial.print("ACTIVATION: "); Serial.println(Mb.R[ACTIVE]);
+  Serial.println();
 }
-
-boolean deButton (int pin, boolean state)
-{
-  boolean stateNow = digitalRead(pin);
-  if(state==stateNow) 
-  {
-    delay(10);
-    return HIGH;
-  } 
-  else return LOW;
-}
-
