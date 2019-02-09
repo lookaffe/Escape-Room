3fd73fa5da1a20209859965a605b0a5319cec2cf
lookaffe <lookaffe@gmail.com>
2019-02-09 15:40:41 +0100

    totem quasi

--- Software/Room2/G6_Totem/2xHCSR04_Test2/2xHCSR04_Test2.ino
+++ Software/Room2/G6_Totem/2xHCSR04_Test2/2xHCSR04_Test2.ino
@@ -3,6 +3,27 @@
   3/26/17 by Brian Patton
   Feel free to do whatever you want with this code example
 */
+
+
+//#define ONLINE
+
+#define SENNUM  2       //total amount of sensors
+#define ACTNUM  1       //total amount of actuators
+#define DEVNUM  2       //total amount of internal devices
+#define ALWAYSACTIVE 1  //1 if the game is always active
+
+const int senPins[SENNUM] = {21, 23}; // Ultrasuono alto, Ultrasuono basso
+const int actPins[ACTNUM] = {2}; // elettrovalvola nuvola
+const int devPins[DEVNUM] = {0, 1}; // luce alta, luce bassa
+
+uint8_t mac[] = {0x04, 0xE9, 0xE5, 0x06, 0xE1, 0x29}; //Dipende da ogni dispositivo, da trovare con T3_readmac.ino (Teensy) o generare (Arduino)
+uint8_t ip[] = {10, 0, 1, 106};                     //This needs to be unique in your network - only one puzzle can have this IP
+
+void resetSpec() {
+}
+
+#include <EscapeFunction.h>
+
 byte trigPin[2] = {20, 22};  // Trigger is connected to this pin
 byte echoPin[2] = {21, 23};   // Echo is connected to this pin
 float echoTime;       // Variable to store echo pulse time
@@ -13,6 +34,7 @@ float distance;       // Variable to hold calculated distance
 //                Setup
 //************************************************************
 void setup() {
+  setupEscape();
   pinMode(trigPin[0], OUTPUT);   // Set pin to Output
   pinMode(trigPin[1], OUTPUT);   // Set pin to Output
   pinMode(echoPin[0], INPUT);    // Set pin to Intput
@@ -25,11 +47,14 @@ void setup() {
 //                Main Loop
 //************************************************************
 void loop() {
+  Mb.Run();
+  listenFromEth();
   for (int i = 0; i < 2; i++) {
     distance = echoCM(i);    // Call non-void function to get the distance in cm
     Serial.println("Distance in Cms " + (String)i + "= " + (String)distance);    // print data
     delay(300);             // Delay 1/2 second
   }
+  printRegister();
 }
 //************************************************************
 //                Calculate distance 1 in cm
--- Software/Room2/G6_Totem/G6_Totem/G6_Totem.ino
+++ Software/Room2/G6_Totem/G6_Totem/G6_Totem.ino
@@ -1,6 +1,6 @@
 //Teensy 3.2
 
-#define ONLINE
+//#define ONLINE
 
 #define SENNUM  2       //total amount of sensors
 #define ACTNUM  1       //total amount of actuators
@@ -14,7 +14,10 @@ const int devPins[DEVNUM] = {0, 1}; // luce alta, luce bassa
 uint8_t mac[] = {0x04, 0xE9, 0xE5, 0x06, 0xE1, 0x29}; //Dipende da ogni dispositivo, da trovare con T3_readmac.ino (Teensy) o generare (Arduino)
 uint8_t ip[] = {10, 0, 1, 106};                     //This needs to be unique in your network - only one puzzle can have this IP
 
+int stato = 0;
+
 void resetSpec() {
+  stato = 0;
 }
 
 byte trigPin[2] = {20, 22};  // Trigger is connected to this pin
@@ -24,11 +27,15 @@ float CMs;            // Variable to carry Cm values
 
 #include <EscapeFunction.h>
 
-const int distance = 70;
-int stato = 0;
-elapsedMillis lap = 0;
+const float distance = 70.00;
+
+elapsedMillis lapUp, lapDown = 0;
 const long lapTime = 3000;
-elapsedMillis danceTime = 10000;
+elapsedMillis danceUp, danceDown = 0;
+const long danceTime = 10000;
+int count = 0;
+
+bool is = LOW;
 
 void setup()
 {
@@ -50,36 +57,65 @@ void loop()
     isPuzzleSolved();
   }
   printRegister();
+  Serial.println(); Serial.println(); Serial.println(); Serial.println(); Serial.println(); Serial.println();
 }
 
 void gameUpdate() {
-  Serial.println("Stato " + (String)stato);
+  float dist[2] = {0.0, 0.0};
+  for (int i = 0; i < 2; i++) {
+    dist[i] = echoCM(i);    // Call non-void function to get the distance in cm
+    myDelay(100);             // Delay 1/2 second
+  }
+  Serial.println("lapUp " + (String)lapUp);
   switch (stato) {
     case 0:
       stato = Mb.R[ACTIVE];
-      lap = 0;
+      lapUp = 0;
+      danceUp = 0;
       break;
     case 1:
-      Serial.println("lap: " + (String)lap + " - lapTime: " + (String)lapTime);
-      if ((echoCM(1))) {
-        if (lap < lapTime) sensorRegUpdate(0, HIGH);
-        else sensorRegUpdate(0, LOW);
-        lap = 0;
+      if (dist[0] < distance) { // 1. se passo davanti al sensore alto
+        if (lapUp < lapTime) { // 3. se il tempo del giro è minore di quello stabilito (non sei lento)
+          if (danceUp > danceTime) {           
+            stato = 2; // 4. Se il tempo di danza è maggiore di quello stabilito (hai ballato abbastanza)
+            lapDown = 0; 
+            danceDown = 0;
+          }
+        } else {
+          danceUp = 0; // 5. altrimenti aggiorno il tempo di ballo
+        }
+        lapUp = 0; // 2. comunque aggiorno il tempo del giro
       }
+      sensorRegUpdate(0, stato);
+      break;
+    case 2:
+      if (dist[0] < distance && lapUp > 1500) stato = 1; // 0. se passo davanti al sensore alto, ritorno allo stato 1
+      if (dist[1] < distance) { // 1. se passo davanti al sensore basso
+        if (lapDown < lapTime) { // 3. se il tempo del giro è minore di quello stabilito (non sei lento)
+          if (danceDown > danceTime) {
+            puzzleSolved = 1;  // 4. Se il tempo di danza è maggiore di quello stabilito (hai ballato abbastanza)
+          }
+        } else {
+          danceDown = 0; // 5. altrimenti aggiorno il tempo di ballo
+        }
+        lapDown = 0; // 2. comunque aggiorno il tempo del giro
+      }
+      sensorRegUpdate(1, is);
 
-      Serial.println("Distance in Cms " + (String)1 + "= " + (String)CMs);    // print data
-      myDelay(300);             // Delay 1/2 second
+      break;
   }
+
 }
 
-bool echoCM(int n) {
+float echoCM(int n) {
   digitalWrite(trigPin[n], LOW);  // Make sure the pin starts off low
-  delay(1);                    // Min between triggers is 50us so let's do 1000us
+  delayMicroseconds(2);                    // Min between triggers is 50us so let's do 1000us
   digitalWrite(trigPin[n], HIGH); // Set trigger pin High for....
-  delayMicroseconds(10);       // 10 microseconds.
+  delayMicroseconds(5);       // 10 microseconds.
   digitalWrite(trigPin[n], LOW);  // Set it low again
 
   echoTime = pulseIn(echoPin[n], HIGH);  //Collect the echo pulse
   CMs = echoTime / 58;                //Conversion in Data Sheet
-  return ((CMs < distance) ? true : false);
+  Serial.println("Distance in Cms " + (String)n + "= " + (String)CMs);    // print data
+  return CMs;
 }
